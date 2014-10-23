355a7a3ce0edc052da6ffbef3b6dcb3e0d494287
collin <collin@corephp.com>
2014-10-23 18:18:10 -0400

    adding project and adding lightbox

--- js/script.js
+++ js/script.js
@@ -1,11 +1,56 @@
-$.noConflict();
-
 jQuery(function ($) {
 
-   $('.lightbox').on('click', function(){
-   		var that = $(this),
-   			src = that.attr('href');
-   });
+	///////////////////////////////////////////////
+	// Lightbox Start /////////////////////////////
+	///////////////////////////////////////////////
 
+	//Add empty lightbox container to body
+	if ( $('.lightbox').length > 0 ) {
+		$('body').append('<div id="lightbox"><div class="container"></div></div>');
+	}
+
+	//Open lightbox on link click
+	$('.lightbox').on('click', function(event){
+
+		//Stop browser from redirecting to new page
+		event.preventDefault();
+
+		var that = $(this),
+			src = that.attr('href');
+
+		//Get contents of page
+		$.ajax({
+			url: src,
+			context: document.body,
+			cache: false
+		//What do with contents? Insert them into lightbox
+		}).done(function(data){
+			var box = $('#lightbox'),
+				container = $('#lightbox > .container'),
+				toTop = $(window).scrollTop();
+			container.html('');
+			container.append(data + '<span class="lightbox-exit btn btn-warning"><i class="fa fa-close"></i>Esc</span>');
+			box.addClass('show');
+			$('body').addClass('lightbox-active');
+			//Exit Button
+			$('.lightbox-exit').on('click', function(){
+				$('body').removeClass('lightbox-active');
+				$('#lightbox').removeClass('show');
+				$(window).scrollTop(toTop);
+			});
+			//Exit with ESC
+			$(document).on('keyup', function(e){
+				if (e.keyCode == 27) {
+					$('body').removeClass('lightbox-active');
+					$('#lightbox').removeClass('show');
+					$(window).scrollTop(toTop);
+				}
+			});
+		});
+	});
+
+	///////////////////////////////////////////////
+	// Lightbox Start End /////////////////////////
+	///////////////////////////////////////////////
 
 });
